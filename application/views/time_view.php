2b532ceaffeaf97937f3d7d411b9e0e6080b202e
Koddi <nazgardo@ya.ru>
2014-02-09 15:32:28 +0600

    2014-02-09/2

--- application/views/time_view.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <p>
-        <span style="color: gray">
-            <?
-            echo 'Время сервера: ' . strftime('%A %F %H:%M:%S', time());
-            ?>
-        </span>
-    </p>
-</div>
