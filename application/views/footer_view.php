4288d06da97c57fc75f832919f556358f4857ea8
Nazgard <nazgardo@ya.ru>
2014-02-13 16:07:42 +0600

    view memory_usage on footer

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="well">
         <div class="pull-right">
-            Page rendered in <strong>{elapsed_time}</strong> seconds
+            Page rendered in <strong>{elapsed_time}</strong> seconds. Memory usage <strong>{memory_usage}</strong>
         </div>
         Created by Makarov<br>
         <a href="mailto:nazgardo@ya.ru">E-mail</a>&nbsp;|&nbsp;<a href="https://github.com/Nazgard?tab=repositories">GitHub</a>&nbsp;|&nbsp;<a
